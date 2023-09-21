df1fd8199bdd928d24ec6c750d08c8b6dced11d8
Your Name <your-email@example.com>
2023-09-21 14:01:45 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import './Server'
 function App() {
   return (
     <>
@@ -10,11 +12,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/Vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
       </Routes>
     </>
   )
