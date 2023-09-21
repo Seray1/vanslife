cbc67a0af80c2f5665af6d131a317a1b14d0f2ec
Your Name <your-email@example.com>
2023-09-21 14:43:15 +0100

    update

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Vans() {
+  const [vans, setVans] = useState([])
+  useEffect(() => {
+    fetch('/api/vans')
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans))
+  }, [])
+  console.log(vans)
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ))
   return (
-    <div>
-      <h1>van page goes here</h1>
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
     </div>
   )
 }
