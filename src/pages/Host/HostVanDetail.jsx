90c0c4e06741414f776364695cbf1dba149df469
Your Name <your-email@example.com>
2023-09-26 00:44:39 +0100

    update

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 function HostVanDetail() {
+  const params = useParams()
+  const [currentVan, setCurrentVan] = useState(null)
+  useEffect(() => {
+    fetch(`/api/host/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans))
+  }, [])
+  if (!currentVan) {
+    return <h1>Loading...</h1>
+  }
   return (
     <div>
-      <h1>host van detail</h1>
+      <img src={currentVan.imageUrl} width={150} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
     </div>
   )
 }
